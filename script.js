b3f81bded5d55e31ac83c3d9048caabd5b777dae
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2023-03-08 16:54:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let result = document.getElementById('result');
 
 function getResult(value) {
-   if (value === '=') {
-     result.value = eval(result.value);
-   } else {
-     result.value += value;
-   }
+  if (value === '=') {
+    result.value = eval(result.value);
+  } else {
+    result.value += value;
+  }
 }
 
-function clearResult() {
-   result.value = '';
+function clearAll() {
+  result.value = '';
+}
+
+function removeLastChar() {
+  result.value = result.value.slice(0, -1);
 }
