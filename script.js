df9b54bfc6d9e4652a4a36fc0b28020112c475eb
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2023-03-10 09:16:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-const gpu = new GPU();
-
-const getResult = gpu.createKernel(function(input) {
-  let result = '';
-  if (input[this.thread.x] === '=') {
-    result = eval(this.output[0]);
-  } else {
-    result = this.output[0] + input[this.thread.x];
+const hasGPU = () => {
+  try {
+    const gpu = new GPU();
+    return true;
+  } catch (e) {
+    return false;
   }
-  return result;
-}).setOutput([1]);
+}
 
-const clearAll = gpu.createKernel(function() {
-  return '';
-}).setOutput([1]);
+let getResult, clearAll, removeLastChar;
 
-const removeLastChar = gpu.createKernel(function(input) {
-  return input.slice(0, -1);
-}).setOutput([1]);
+if (hasGPU()) {
+  const gpu = new GPU();
+  getResult = gpu.createKernel(function(input) {
+    let result = '';
+    if (input[this.thread.x] === '=') {
+      result = eval(this.output[0]);
+    } else {
+      result = this.output[0] + input[this.thread.x];
+    }
+    return result;
+  }).setOutput([1]);
+
+  clearAll = gpu.createKernel(function() {
+    return '';
+  }).setOutput([1]);
+
+  removeLastChar = gpu.createKernel(function(input) {
+    return input.slice(0, -1);
+  }).setOutput([1]);
+} else {
+  getResult = (input) => {
+    let result = '';
+    if (input === '=') {
+      result = eval(result.value);
+    } else {
+      result = result.value + input;
+    }
+    return result;
+  }
+
+  clearAll = () => {
+    result.value = '';
+  }
+
+  removeLastChar = () => {
+    result.value = result.value.slice(0, -1);
+  }
+}
